b82620f39c4a4e1c0ff175d194b473f07e023d0b
Arash-11 <arashnawy@gmail.com>
2020-12-03 20:47:50 -0400

    updated navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,8 @@ function Navbar() {
 
     const [selectedTab, setSelectedTab] = useState();
 
+    const styling = {fontWeight: '600', color: 'white', opacity: '1'};
+
     useEffect(() => {
         const tab = (window.location.pathname).slice(1);
         setSelectedTab(tab);
@@ -22,19 +24,19 @@ function Navbar() {
             <nav className="header__nav">
                 <Link to='/' onClick={highlightTab} className="header__nav__title"> HN </Link>
                 <div className="header__nav__items">
-                    <Link to='/top' onClick={highlightTab} className={selectedTab === 'top' ? 'highlight' : ''}>
+                    <Link to='/top' onClick={highlightTab} style={selectedTab === 'top' ? styling : {color: ''}}>
                         top
                     </Link>
-                    <Link to='/new' onClick={highlightTab} className={selectedTab === 'new' ? 'highlight' : ''}>
+                    <Link to='/new' onClick={highlightTab} style={selectedTab === 'new' ? styling : {color: ''}}>
                         new
                     </Link>
-                    <Link to='/ask' onClick={highlightTab} className={selectedTab === 'ask' ? 'highlight' : ''}>
+                    <Link to='/ask' onClick={highlightTab} style={selectedTab === 'ask' ? styling : {color: ''}}>
                         ask
                     </Link>
-                    <Link to='/show' onClick={highlightTab} className={selectedTab === 'show' ? 'highlight' : ''}>
+                    <Link to='/show' onClick={highlightTab} style={selectedTab === 'show' ? styling : {color: ''}}>
                         show
                     </Link>
-                    <Link to='/jobs' onClick={highlightTab} className={selectedTab === 'jobs' ? 'highlight' : ''}>
+                    <Link to='/jobs' onClick={highlightTab} style={selectedTab === 'jobs' ? styling : {color: ''}}>
                         jobs
                     </Link>
                 </div>
